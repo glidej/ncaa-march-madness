9166b97f49ffb2fd9253b3dd6d9d3493a29daaf5
Jacob <glidej@gmail.com>
2017-03-14 10:42:01 -0400

    Update matchups to 2017 tournament

--- app/services/first-round.js
+++ app/services/first-round.js
@@ -2,103 +2,100 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   matchups: [
-    "d08152ed-57f4-405d-a848-b6ccbbd59602",
-    "f131dc08-111b-40bb-aca8-2b9c866a44ad",
+    "ff39a4e3-2cc6-48f5-ac58-ff3b661328bb", // Villanova Wildcats
+    "120499c4-92d6-48db-90e9-d27ac8a6fb0b", // New Orleans Privateers (TBD)
 
-    "848a5afd-9068-46c0-8c34-42bb43ca5fc7",
-    "92c59ce2-f96e-4a7e-91af-110f5f445480",
+    "dd62793f-2ef2-468f-8288-3363f3b497e3", // Wisconsin Badgers
+    "8b2a6fc4-7ada-4141-a49b-d00f767a6076", // Virginia Tech Hokies
 
-    "6433e300-0311-4d18-ac21-9da06c9de896",
-    "864574ae-990b-4b59-8e30-243064daeb56",
+    "ba7f81e4-b81a-431f-afaf-3f960d2dc4c2", // Virginia Cavaliers
+    "579d19cc-446b-4e0c-8e47-a0add46e1a59", // North Carolina-Wilmington Seahawks
 
-    "00600212-c227-4eee-bfe8-1ab3791332fd",
-    "e3f3ca7d-157d-4cd6-9eec-c2a9743ab5ad",
+    "64cd5b91-a527-4af5-9f9c-1fa4958ea59a", // Florida Gators
+    "12f0400f-c2ed-4fd7-bd9c-a21b7c85db16", // East Tennessee State Buccaneers
 
-    "7a7bbcd4-1363-43ca-a503-b65863528a6a",
+    "22502872-46fe-4358-97de-3e5071d4f24d", // Southern Methodist Mustangs
+    "c52d8366-c94a-497c-9975-39c4a651fa72", // USC Trojans (TBD)
 
-    "d2643a63-08b4-4d33-a064-a7ee1c3785b6",
+    "087000c8-44ac-4177-9040-34de231ccc81", // Baylor Bears
+    "fe8b3bd5-c5da-403a-9fe8-58a6e32a7b3f", // New Mexico State Aggies
 
-    "50d5ddaf-91fa-472e-8ef0-52eeec08cfe4",
-    "58e0119d-5808-44b4-903b-ffdd018eec69",
+    "0eca9615-1c19-489d-ae21-7a8288e9dee6", // South Carolina Gamecocks
+    "c7451335-082b-41cd-b074-aab88b93baa7", // Marquette Golden Eagles
 
-    "78456aee-253f-412c-9ca4-afd5b3318d2a",
-    "24b074a7-96e0-4cd9-86c7-4c67d5011418",
+    "e7f3d873-683a-41de-bd31-cdd3a8d7a6eb", // Duke Blue Devils
+    "e97a5008-6e84-45b0-9f3d-5595b8bf5196", // Troy Trojans
 
-    "ff39a4e3-2cc6-48f5-ac58-ff3b661328bb",
-    "d750a8d9-5a49-4020-82ab-f3fedb34d65c",
+    "92eceb5b-61e4-4abb-81e2-b8cd727d3f65", // Gonzaga Bulldogs
+    "864574ae-990b-4b59-8e30-243064daeb56", // South Dakota State Jackrabbits
 
-    "07a7134c-64e8-4456-bb44-1bf12ddddc12",
+    "07214056-ba88-450a-bf71-228112a2ee14", // Northwestern Wildcats
+    "b00f3d13-096a-4668-a668-328bfc8884fb", // Vanderbilt Commodores
 
-    "3641e0ce-f998-4cb4-85f2-05667fd97659",
+    "11e6ea12-deb8-40aa-9b42-f2ec45a33fbc", // Notre Dame Fighting Irish
+    "313aad19-864f-499d-aa0e-cbca54141a21", // Princeton Tigers
 
-    "0558c5c6-3cdf-419a-b5a3-b3ee33518838",
-    "07441489-67ed-4693-a7e9-6220332412be",
+    "8347b465-296e-4bad-a97a-f6c489f9a5c8", // West Virginia Mountaineers
+    "34bc136b-c3a7-42cd-bc18-376c89cbb368", // Bucknell Bison
 
-    "087000c8-44ac-4177-9040-34de231ccc81",
-    "9e8774fc-2f02-40c2-9ae3-f83fa680cd5d",
+    "6433e300-0311-4d18-ac21-9da06c9de896", // Maryland Terrapins
+    "ec4c5602-c04c-4aae-af22-b71d69b285ae", // Xavier Musketeers
 
-    "e7f3d873-683a-41de-bd31-cdd3a8d7a6eb",
-    "579d19cc-446b-4e0c-8e47-a0add46e1a59",
+    "d5fad8e8-d529-4092-aaf6-dd80aea9b6e6", // Florida State Seminoles
+    "256fec21-0f69-4ba0-8c6d-6d70a8ce1229", // Florida Gulf Coast Eagles
 
-    "87a426ab-cbd5-4c6b-bc13-bd4f37ff29a2",
-    "15e07161-8b8d-4092-ad19-7550261b9544",
+    "28fb0367-7c88-4737-82cb-f5c2ad126b17", // Saint Mary's Gaels
+    "dab50263-170c-428b-aab8-32729d58d927", // Virginia Commonwealth Rams
 
-    "60c0d70c-c7f7-43ff-8fb0-f404397fa2dd",
-    "972077dd-daab-41c8-bcde-8aa6426a8558",
+    "7a7bbcd4-1363-43ca-a503-b65863528a6a", // Arizona Wildcats
+    "f97a4f08-64f4-47ea-8565-7087d442abdd", // North Dakota Fighting Hawks
 
-    "a3ee1f58-fbb8-4af1-8db6-c85720cb4952",
-    "dab50263-170c-428b-aab8-32729d58d927",
+    "d08152ed-57f4-405d-a848-b6ccbbd59602", // Kansas Jayhawks
+    "c6361ba8-5b0d-4636-9cc9-efe4aa673f09", // UC Davis Aggies (TBD)
 
-    "3c8b3941-4f2d-4eb9-8bdc-8bb95590962b",
-    "665af254-4fab-435b-b239-473e44e11592",
+    "50d5ddaf-91fa-472e-8ef0-52eeec08cfe4", // Miami (FL) Hurricanes
+    "2aee418d-168d-4754-bae7-06f0f46391b1", // Michigan State Spartans
 
-    "8ab4c96f-ba7a-4e0d-a773-91dd64e36e25",
-    "256fec21-0f69-4ba0-8c6d-6d70a8ce1229",
+    "4a6d609b-9288-46dc-87b2-4033541db56a", // Iowa State Cyclones
+    "d7040143-31b9-40f4-a331-8d8f2b125ff7", // Nevada Wolf Pack
 
-    "c52d8366-c94a-497c-9975-39c4a651fa72",
-    "df29d106-8c22-4f23-ae7c-7e216cdbbba2",
+    "2038d0d4-b5f3-4080-927c-4b059cd7f801", // Purdue Boilermakers
+    "d0ecbed9-a7bd-4953-bbe8-9e18fdfc24b4", // Vermont Catamounts
 
-    "2fe72ee5-efc5-4ffb-88ef-61bd0df77458",
-    "b1e06643-b630-4c77-b951-33509df4424a",
+    "e56f93ff-3f6d-4914-853d-c8d8b871fd4f", // Creighton Bluejays
+    "918381d0-4820-4b27-bc4c-6c701cb19875", // Rhode Island Rams
 
-    "6e0969f7-ff48-4cba-94be-666fe125f150",
-    "5a0b67d6-d0f8-4f27-a07f-86a776d99c12",
+    "07a7134c-64e8-4456-bb44-1bf12ddddc12", // Oregon Ducks
+    "49b5f641-3d38-40a7-970f-59b91d02cca7", // Iona Gaels
 
-    "11e6ea12-deb8-40aa-9b42-f2ec45a33fbc",
+    "cec76c74-652b-4833-b40a-acab06687205", // Michigan Wolverines
+    "7918ea86-51df-49c0-a51f-74265059d7c8", // Oklahoma State Cowboys
 
-    "cec76c74-652b-4833-b40a-acab06687205",
+    "f98bc42b-c76f-4b87-951d-3ff318095a4c", // Louisville Cardinals
+    "459d12b3-8764-47db-9620-3de23c48910b", // Jacksonville State Gamecocks
 
-    "8347b465-296e-4bad-a97a-f6c489f9a5c8",
-    "496fcdc9-25bb-4c9d-9c97-b6cce1da5dbe",
+    "8ab4c96f-ba7a-4e0d-a773-91dd64e36e25", // North Carolina Tar Heels
+    "04144e1d-e0c0-466e-88a4-a34acb39c21b", // Texas Southern Tigers
 
-    "dd62793f-2ef2-468f-8288-3363f3b497e3",
-    "70425216-c0b3-419c-ae0b-aea45921c5bb",
+    "90c43bd9-c7b0-46e3-b42b-c7d5f7f36a62", // Arkansas Razorbacks
+    "86bd6eea-e554-4e17-a82e-2fe9d835c7db", // Seton Hall Pirates
 
-    "ec4c5602-c04c-4aae-af22-b71d69b285ae",
-    "0503528e-5164-47f8-8399-9c06dd5137cc",
+    "31017841-4d59-4c3f-97d3-e1462c3d6583", // Minnesota Golden Gophers
+    "10b0290f-a7cd-42aa-bc83-05c1fdd68dd1", // Middle Tennessee Blue Raiders
 
-    "ba7f81e4-b81a-431f-afaf-3f960d2dc4c2",
-    "5aa4d8ec-d7b4-46c7-aba4-60d5ddf33125",
+    "c2ed6884-5412-4a13-bf21-f89adff8e715", // Butler Bulldogs
+    "6649ba1e-ab1c-49ca-8943-9a1ee3fd0b5b", // Winthrop Eagles
 
-    "bc1ab8f2-05be-4825-85ff-3f38d1bec53e",
-    "c2ed6884-5412-4a13-bf21-f89adff8e715",
+    "07441489-67ed-4693-a7e9-6220332412be", // Cincinnati Bearcats
+    "4bec8dec-c250-42c3-9fb0-e545aea5066f", // Wake Forest Demon Deacons (TBD)
 
-    "2038d0d4-b5f3-4080-927c-4b059cd7f801",
-    "b3f185cf-f53d-4974-a720-ad9bcf7b61bf",
+    "e7384e50-1add-4337-8094-063a6ba07e4a", // UCLA Bruins
+    "859c2684-5c7f-416c-a965-84b4a30cb65a", // Kent State Golden Flashes
 
-    "4a6d609b-9288-46dc-87b2-4033541db56a",
-    "49b5f641-3d38-40a7-970f-59b91d02cca7",
+    "4b467a31-313b-4d8b-9b63-14cb2503c909", // Dayton Flyers
+    "d2643a63-08b4-4d33-a064-a7ee1c3785b6", // Wichita State Shockers
 
-    "86bd6eea-e554-4e17-a82e-2fe9d835c7db",
-    "92eceb5b-61e4-4abb-81e2-b8cd727d3f65",
-
-    "4751100f-2ae0-40a0-a7a6-39370bdb83e1",
-    "6eb976d6-4125-474b-9f44-2ac0214dceab",
-
-    "4b467a31-313b-4d8b-9b63-14cb2503c909",
-    "f0acf0d7-607f-4493-bba7-bc5e557b376e",
-
-    "2aee418d-168d-4754-bae7-06f0f46391b1",
-    "10b0290f-a7cd-42aa-bc83-05c1fdd68dd1"
+    "6e0969f7-ff48-4cba-94be-666fe125f150", // Kentucky Wildcats
+    "17c7b6e6-8141-489d-bf35-a1a8b49d5eb6"  // Northern Kentucky University Norse
   ]
 });
