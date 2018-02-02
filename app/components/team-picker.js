daf9192e7ba2c9a9f19a60a8f23825ca16e8ddd4
Jacob Glide <jacob.glide@detroitlabs.com>
2018-02-02 14:35:00 -0500

    feat: sanitize inline style assignment

--- app/components/team-picker.js
+++ app/components/team-picker.js
@@ -1,18 +1,25 @@
 import Component from '@ember/component';
 import { computed, get } from '@ember/object';
+import { htmlSafe } from "@ember/string"
 
 export default Component.extend({
-
   selectedTeam: null,
+  sanitize(team) {
+    return Object.assign({}, team, {
+      colors: (team.colors || []).map((color) => {
+        return htmlSafe(`background-color: ${color}`);
+      })
+    });
+  },
 
   left: computed('matchup.[]', {
     get() {
-      return this.get('matchup').objectAt(0);
+      return this.sanitize(this.get('matchup').objectAt(0));
     }
   }),
   right: computed('matchup.[]', {
     get() {
-      return this.get('matchup').objectAt(1);
+      return this.sanitize(this.get('matchup').objectAt(1));
     }
   }),
 
